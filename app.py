f0fea882b24b8603553dadb36f0f0bb9af33b3c7
Tae-Wha <twfluti@gmail.com>
2026-04-16 09:31:26 +0200

    Lagt till features. Implementerat sökfunktion och dynamiska rutter för kategorier i Flask

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/search")
+def search():
+    """ 
+    Hämtar det användaren skrev i rutan med name="q"
+    """
+    query = request.args.get('q')
+    return f"<h1>Sökresultat</h1><p>Du söker efter: {query}</p><a href='/'>Tillbaka till start</a>"
+
+
+@app.route("/kategori/<namn>")
+def kategori(namn):
+    """
+    Här kan man senare lägga till logik för att visa olika maträtter
+    """
+    return f"<h1>Välkommen till {namn}</h1><p>Här kommer vi visa alla tjänster inom {namn}.</p>"
+
+
+@app.route("/alla-kategorier")
+def alla_kategorier():
+    """
+    En specifik sida för HTML classen 'card-all'
+    """
+    return "<h1>Här listas alla våra catering-kategorier</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
