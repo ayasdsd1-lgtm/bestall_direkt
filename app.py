68be3cd6f28497d5bf2d8b0f3108defdc7f9233a
majaahlm <maja.ahlm@hotmail.se>
2026-04-20 03:37:52 +0200

    Update app.py
    Databasen

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="Bestall_Direkt",
+    user="postgres",
+    password="1234",
+    host="localhost",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -30,6 +43,24 @@ def alla_kategorier():
     En specifik sida för HTML classen 'card-all'
     """
     return "<h1>Här listas alla våra catering-kategorier</h1>"
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form["email"]
+    password = request.form["password"]
+
+    cursor.execute(
+        "SELECT * FROM foretagare WHERE email = %s AND losenord = %s",
+        (email, password)
+    )
+
+    user = cursor.fetchone()
+
+    if user:
+        return "Inloggad"
+    else:
+        return "Fel uppgifter"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
