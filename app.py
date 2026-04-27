678e404c6780423f96144ae205263c3e3a1b99ab
Aya <ayasdsd1@gmail.com>
2026-04-27 13:37:07 +0200

    python och js
    rättade små fel som gjorde att sidan kraschade

--- app.py
+++ app.py
@@ -82,8 +82,8 @@ def view_company(company_id):
         "epost": "ajabaja@ajabaja.com",
         "beskrivning": "Gottegott gottegott",
         "tjanster":[
-            {"namn": "Dolma", "pris": "10kr st"},
-            {"namn": "Mini cheesecake", "pris": "20kr st"}
+            {"namn": "Dolma", "pris": 10},
+            {"namn": "Mini cheesecake", "pris": 20}
         ]
     }
     return render_template("view.html", foretag=test_data)
--- static/script.js
+++ static/script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     let total = 0;
-    const button = document.querySelectorAll(' .add-btn');
+    const buttons = document.querySelectorAll(' .add-btn');
     const totalDisplay = document.getElementById('total-summa');
 
 
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             const pris = parseFloat(this.getAttribute('data-pris'));
-            const namn = this.getAttribute('.add-namn');
+            const namn = this.getAttribute('.data-namn');
             total += pris;
             totalDisplay.textContent = total.toLocaleString('sv-SE');
-            console.log('${namn} lades till för ${pris}kr');
+            console.log(`${namn} lades till för ${pris}kr`);
             this.style.backgroundColor = "#4CAF50";
             this.textContent = "Tillagd!";
             setTimeout (() => {
