0d685126a99ad9645fda3f7afc9a70deaa3bb3fd
Aya <ayasdsd1@gmail.com>
2026-04-28 08:58:32 +0200

    script.js
    prisöversikt

--- static/script.js
+++ static/script.js
@@ -1,22 +1,52 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let total = 0;
+    let totalSumma = 0;
+    let totalAntal = 0;
+    const varukorg = {}; 
+
     const buttons = document.querySelectorAll(' .add-btn');
-    const totalDisplay = document.getElementById('total-summa');
+    const totalDisplay = document.getElementById('total-summa')
+    const antalDisplay = document.getElementById('antal-produkter')
+    const listaDisplay = document.getElementById('produkt-lista')
 
 
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             const pris = parseFloat(this.getAttribute('data-pris'));
             const namn = this.getAttribute('.data-namn');
-            total += pris;
-            totalDisplay.textContent = total.toLocaleString('sv-SE');
-            console.log(`${namn} lades till för ${pris}kr`);
+
+            totalSumma += pris;
+            totalAntal += 1;
+
+            if (varukorg[namn]) {
+                varukorg[namn] += 1;
+            } else {
+                varukorg[namn] = 1;
+            }
+
+            uppdateraPrisoversikt();
             this.style.backgroundColor = "#4CAF50";
             this.textContent = "Tillagd!";
             setTimeout (() => {
                 this.style.backgroundColor = "";
                 this.textContent = "Lägg till";
-            }, 1000);
-        })
+            }, 800);
+        });
     });
-})
+
+
+    function uppdateraPrisoversikt() {
+        totalDisplay.textContent = totalSumma.toLocaleString('sv-SE');
+        antalDisplay.textContent = totalAntal
+
+        listaDisplay.innerHTML = "";
+        for (const [namn, anyal] of Object.entries(varukorg)) {
+            const li = document.createAttribute('li');
+            li.style.display = "flex";
+            li.style.justifyContent = "space-between";
+            li.style.marginBotoom = "5px";
+
+            li.innerHTML = `<span>${namn}</span> <span>${antal} st<span>`;
+            listaDisplay.appendChild(li);
+        }
+    }
+});
